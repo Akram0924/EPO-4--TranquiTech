a11552bf2e0e5bb5adf2020ec0f24dde6f2c19a5
Tomasz2003 <Tabels@tudelft.nl>
2023-05-10 14:53:41 +0200

    Update Biosensing.py

--- Biosensing.py
+++ Biosensing.py
@@ -4,6 +4,7 @@ import numpy as np
 from scipy import signal
 from scipy.signal import butter, iirnotch, lfilter
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt   #importing all the libaries
 
+#now we open the dataset
 
