44116614ac2195732e3e0462dd2d0b64ace64286
tjgeschiere <thijsgeschiere@hotmail.com>
2023-05-31 17:22:55 +0200

    commits

--- Matrix_Creation.py
+++ Matrix_Creation.py
@@ -21,7 +21,24 @@ def create_array(n_features, n_subjects):
 
 df = create_array(4,3)
 #%%
-df
+df["ID"][0]
+#%%
+def train_test_split(df, test_ID):
+    train = []
+    X_test = []
+    Y_train = []
+    test = []
 
+    #for i in range(df["ID"].nunique()):
+    for o in range(df.shape[0]):
+        if df["ID"][o] == test_ID:
+            test.append(np.array(df.iloc[o]))
+        else:
+            train.append(np.array(df.iloc[o]))
     
+    return train, test
+train, test = train_test_split(df, 1)
+
+# %%
+test.length
 # %%
